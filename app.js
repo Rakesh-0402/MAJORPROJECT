337116140300fb4eb365c96eecd7f33b3d9070a8
Rakesh <rk92057sh@gmail.com>
2025-08-25 00:00:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
- require('dotenv').config();
-console.log(process.env.SECRET);
+require('dotenv').config();
 const express=require("express");
 const app=express();
 const port=process.env.PORT || 8080;
@@ -12,9 +11,14 @@ const ejsMate =require("ejs-mate");
 const passport =require("passport");
 const LocalStrategy=require("passport-local");
 const User =require("./models/user.js");
-//cookies
-const cookieParser = require("cookie-parser");
-app.use(cookieParser("secretCode"));//cookie parser middleware
+const flash = require("connect-flash");
+const session=require("express-session");
+const MongoStore =require('connect-mongo');
+
+// Routes
+const listingRoutes = require("./routes/listings");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/users");
 
 const dbURL= process.env.ATLASDB_URL;
 //connection 
@@ -27,11 +31,21 @@ main()
 async function main() {
   await mongoose.connect(dbURL);
 }
+//EJS setup
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"views"));
+app.engine('ejs', ejsMate);
 
+//Middleware
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"/public")));
+
+//cookies
+const cookieParser = require("cookie-parser");
+app.use(cookieParser(process.env.SECRET));
 
 //sessions
-const session=require("express-session");
-const MongoStore =require('connect-mongo');
 app.use(session({
   secret:process.env.SECRET,
   resave: false,              
@@ -49,18 +63,15 @@ app.use(session({
     touchAfter:24*3600 ,
   })
 }));
-  
-//using passport
-app.use(passport.initialize());//tells express to use passport for authentication
-app.use(passport.session());//Passport use sessions to remember users.
-
-passport.use(new LocalStrategy(User.authenticate()));//This sets up a strategy = “How will we log users in?”
-passport.serializeUser(User.serializeUser())//store only user ID in session
-passport.deserializeUser(User.deserializeUser());//fetch full user from ID in DB
-
 //connect-flash
-const flash = require("connect-flash");
 app.use(flash());
+  
+//passport setup
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser());
 
 //accessing flash messages globally
 app.use((req, res, next) => {
@@ -70,86 +81,24 @@ app.use((req, res, next) => {
     next();
 });
 
-
-
-//require router
-const listings =require("./routes/listing.js");
-const reviews =require("./routes/review.js");
-const users =require("./routes/user.js");
-
-app.set("view engine","ejs");
-app.use(methodOverride('_method'));
-app.set("views",path.join(__dirname,"views"));
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,"/public")));
- app.engine('ejs', ejsMate);
-
 //express router
-app.use("/listings" ,listings);
-app.use("/listings/:id/reviews",reviews)
-app.use("/" ,users);
+app.use("/listings" ,listingRoutes);
+app.use("/listings/:id/reviews",reviewRoutes)
+app.use("/" ,userRoutes);
 
 // error handling middleware
 app.use((err,req,res,next)=>{
     let {statusCode =500 ,message="Something went wrong!"}= err;
-   // res.status(statusCode).send(message);
    res.status(statusCode).render("error.ejs",{message});
 });
-
-//setting a signed cookie in browser
-app.get("/setcookie",(req,res)=>{
-  res.cookie("name","John",{
-    signed:true,
-    httpOnly:true, //security
-    maxAge:1000*60*5
-  });
-  res.send(" signed Cookie has been set!");
-  console.dir(req.cookies);
-})
-
-//reading a cookie
-app.get("/getcookie", (req, res) => {
-    // signed cookies are available in req.signedCookies
-    const user = req.signedCookies.name;
-    if (user) {
-        res.send(`Welcome back, ${user}`);
-    } else {
-        res.send("No valid signed cookie found");
-    }
-});
-
-//deleting a cookie
-app.get("/clearcookie", (req, res) => {
-    res.clearCookie("name");
-    res.send(" signed Cookie has been cleared!");
-});
-
-
-//storing data in session
-app.get("/set-session", (req, res) => {
-    req.session.name = "Rakesh";  // storing data
-    req.session.isLoggedIn = true;
-    res.send("Session data has been saved!");
-});
-
-//accessing session data
-app.get("/get-session", (req, res) => {
-    if (req.session.isLoggedIn) {
-        res.send(`Welcome back, ${req.session.name}`);
-    } else {
-        res.send("No active session found");
-    }
-});
-
+//Root route
 app.get("/", (req, res) => {
-  res.send("✅ Server is running successfully on Render!");
+  res.redirect("/listings");
 });
 
-app.use((req,res,next)=>{
+app.all("*" ,(req,res,next)=>{
   throw new CustomError(404, "Page not found");
 });
-
-
 
 //start server
 app.listen(port,()=>{
