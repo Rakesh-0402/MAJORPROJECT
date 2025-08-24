8ebae50a9534a04ae4fbcd18732159efc90258f8
Rakesh <rk92057sh@gmail.com>
2025-08-24 21:23:06 +0530

    Update reviews.js

--- models/reviews.js
+++ models/reviews.js
@@ -1,21 +1,20 @@
 const mongoose=require("mongoose");
 
 const reviewSchema=new mongoose.Schema({
-    comment:String,
+    comment: {
+        type: String,
+       required: true
+   },
     rating:{
         type:Number,
         min:1,
         max:5
     },
-    createdAt:{
-        type:Date,
-        default:Date.now()
-    },
     author:{
         type:mongoose.Schema.Types.ObjectId,
         ref:"User"
     }
-});
+}, {timestamps :true});
 const Review=mongoose.model("Review",reviewSchema);
 
 module.exports= Review;
